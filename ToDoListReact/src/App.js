905635655cb6f1b377ef0fabcc05c74f679daa2d
Shira <shira93310@gmail.com>
2024-12-24 00:39:33 +0200

    Update App.js

--- ToDoListReact/src/App.js
+++ ToDoListReact/src/App.js
@@ -3,7 +3,7 @@ import service from './service.js';
 
 function App() {
   console.log("REACT_APP_TEST:", process.env.REACT_APP_TEST);
-  console.log("API URL:", process.env.REACT_APP_API_URL);
+  console.log("REACT_APP_API_URL:", process.env.REACT_APP_API_URL);
 
   const [newTodo, setNewTodo] = useState("");
   const [todos, setTodos] = useState([]);
