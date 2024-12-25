8c1ceb99ee9deab5f8c44a26f765dc288a452d65
shira1-git <s0556793310@gmail.com>
2024-12-25 14:05:03 +0200

    try5

--- ToDoListReact/src/service.js
+++ ToDoListReact/src/service.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 // הגדרת כתובת ה-API כ-default
-const apiUrl = "https://todo-list-server-ofps.onrender.com";
+//const apiUrl = "https://todo-list-server-ofps.onrender.com";
 // const apiUrl = process.env.REACT_APP_API_URL;
 
 
-axios.defaults.baseURL = apiUrl;  // הגדרת כתובת ברירת המחדל לכל הקריאות
-console.log("axios.defaults.baseURL",axios.defaults.baseURL)
+//axios.defaults.baseURL = apiUrl;  // הגדרת כתובת ברירת המחדל לכל הקריאות
+//console.log("axios.defaults.baseURL",axios.defaults.baseURL)
 // הוספת interceptor לתפיסת שגיאות
 axios.interceptors.response.use(
   response => response, // אם הקריאה הצליחה, מחזירים את התשובה כרגיל
@@ -20,7 +20,7 @@ export default {
   // שליפת כל המשימות
   getTasks: async () => {
     try {
-      const result = await axios.get("/tasks");
+      const result = await axios.get("https://todo-list-server-ofps.onrender.com/tasks");
       return result.data;
     } catch (error) {
       console.error("Error fetching tasks:", error);
@@ -31,7 +31,7 @@ export default {
   // הוספת משימה חדשה
   addTask: async (name) => {
     try {
-      const result = await axios.post("/tasks", { name, isComplete: false });
+      const result = await axios.post("https://todo-list-server-ofps.onrender.com/tasks", { name, isComplete: false });
       return result.data;
     } catch (error) {
       console.error("Error adding task:", error);
@@ -43,7 +43,7 @@ export default {
 setCompleted: async (id, name, isComplete) => {
   try {
       // שולחים גם את שם המשימה בנוסף לסטטוס
-      const result = await axios.put(`/tasks/${id}`, { name, isComplete });
+      const result = await axios.put(`https://todo-list-server-ofps.onrender.com/tasks/${id}`, { name, isComplete });
       return result.data;
   } catch (error) {
       console.error("Error updating task:", error);
@@ -55,7 +55,7 @@ setCompleted: async (id, name, isComplete) => {
   // מחיקת משימה
   deleteTask: async (id) => {
     try {
-      const result = await axios.delete(`/tasks/${id}`);
+      const result = await axios.delete(`https://todo-list-server-ofps.onrender.com/tasks/${id}`);
       return result.data;
     } catch (error) {
       console.error("Error deleting task:", error);
